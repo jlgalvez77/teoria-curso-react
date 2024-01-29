8cff868ed2b9af07f80f2f5d700a0f7960e25ebb
JoseGalvez <jlgalvez77@gmail.com>
2024-01-29 19:01:55 +0100

    1

---
+++ src/components/Coffe.jsx
@@ -0,0 +1,5 @@
+function Coffe() {
+    return (<h1>Cofffe</h1>)
+  }
+  
+  export default Coffe;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Coffe from './components/Coffe.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Coffe />
+  </React.StrictMode>
+);
+
